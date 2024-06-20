3608f3576172c39f03021138c6c9f99d974e8893
sids7437 <shrtshankar3@gmail.com>
2024-06-20 22:39:58 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM python:3.9-slim-buster
+FROM python:3.8-slim
 
 WORKDIR /app
 
-COPY . /app
+COPY . .
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["python3", "app.py"]
+EXPOSE 5000
+
+ENV FLASK_APP=app.py
+
+CMD ["python", "app.py"]
--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from typing import List
 
-#Declaring variables for setup functions
-PROJECT_NAME="Text to Speech Recognition Project"
-VERSION="0.0.1"
-AUTHOR="Siddharatha Shankar"
-DESRCIPTION="This is a python project of Text to Speech using GTTS"
-
-REQUIREMENT_FILE_NAME="requirements.txt"
-
+# Declaring variables for setup functions
+PROJECT_NAME = "Text to Speech Recognition Project"
+VERSION = "0.0.1"
+AUTHOR = "Siddharatha Shankar"
+DESCRIPTION = "This is a python project of Text to Speech using GTTS"
+REQUIREMENT_FILE_NAME = "requirements.txt"
 HYPHEN_E_DOT = "-e ."
-
 
 def get_requirements_list() -> List[str]:
     """
-    Description: This function is going to return list of requirement
-    mention in requirements.txt file
-    return This function is going to return a list which contain name
-    of libraries mentioned in requirements.txt file
+    Description: This function is going to return list of requirements
+    mentioned in the requirements.txt file.
+    return: This function is going to return a list which contains names
+    of libraries mentioned in requirements.txt file.
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
@@ -26,13 +23,11 @@ def get_requirements_list() -> List[str]:
             requirement_list.remove(HYPHEN_E_DOT)
         return requirement_list
 
-
-
 setup(
-name=PROJECT_NAME,
-version=VERSION,
-author=AUTHOR,
-description=DESRCIPTION,
-packages=find_packages(), 
-install_requires=get_requirements_list()
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
 )
